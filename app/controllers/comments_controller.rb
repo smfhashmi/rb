779bc71db78c10abc6e23bbb31d6c99682a223dd
hashmi mohammed <hashmi.smf@gmail.com>
2024-05-01 12:38:05 +0530

    rerender via turbo streams of new form partials for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,10 +25,21 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
 
     respond_to do |format|
-      if @comment.save
+     if @comment.save
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update('new_comment', partial: 'comments/form', locals: {comment: Comment.new})
+          ]
+        end
         format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
       else
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update('new_comment', partial: 'comments/form', locals: {comment: @comment})
+          ]
+        end
+
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,3 @@
 class Comment < ApplicationRecord
+  validates :title, presence: true
 end
