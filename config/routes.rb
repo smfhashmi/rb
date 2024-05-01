179d0f318bc82462dbedbba2300d04c19cfc320c
hashmi mohammed <hashmi.smf@gmail.com>
2024-05-01 11:14:11 +0530

    added comments scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :todos
   get 'pages/home'
   get 'pages/about'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_27_142827) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_054341) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
