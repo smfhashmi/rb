468738ac8b08b20d662b649392b32b76519318d5
hashmi mohammed <hashmi.smf@gmail.com>
2024-05-01 16:43:13 +0530

    used prepend to render form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
 
   # GET /comments or /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.order(created_at: :desc)
     @comment = Comment.new
   end
 
@@ -28,11 +28,12 @@ class CommentsController < ApplicationController
      if @comment.save
         format.turbo_stream do
           render turbo_stream: [
-            turbo_stream.update('new_comment', partial: 'comments/form', locals: {comment: Comment.new})
+            turbo_stream.update('new_comment', partial: 'comments/form', locals: {comment: Comment.new}),
+            turbo_stream.append('comments', partial: 'comments/comment', locals: {comment: @comment})
           ]
         end
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
-        format.json { render :show, status: :created, location: @comment }
+        #format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
+        #format.json { render :show, status: :created, location: @comment }
       else
         format.turbo_stream do
           render turbo_stream: [
